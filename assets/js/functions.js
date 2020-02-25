dcce0f91d828e7092a6b579694f03af38dcf03b8
Matias Moreira <moreiramatias10@hotmail.com>
2020-02-25 16:44:29 -0300

    agregue funcionabilidades

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,7 +1,7 @@
 $(function () {
 	$('#button--search').click(search);
 	/*$('body').on('click', '#button--search', search);*/
-
+	$('tbody').on('click', '.button--view', view);
 
 
 
@@ -10,7 +10,7 @@ $(function () {
 
 		var query = $('#query').val();
 
-		genius_get('/search', query).done(function(data) {
+		genius_get('/search?q=' + query).done(function(data) {
 
 			printTable(data);
 		
@@ -55,27 +55,52 @@ $(function () {
 		*/	
 	}
 
+
+	function view() {
+		var idSong = $(this).data('song');
+		var idArtist = $(this).data('artist');
+
+		console.log('idSong: ' + idSong + ' idArtist: ' +idArtist);
+
+		genius_get('/songs/' + idSong).done(function(data) {
+			
+			printModal(data);
+
+		});
+	}
+
+	
 	function printTable(data) {
 		
 		var tbody = $('#tbody--query');
 		tbody.html('');
+
 		for (var i = 0; i < data.response.hits.length; i++) {
-			tbody.append('<tr><td>' + data.response.hits[i].result.title + '</td><td>' + data.response.hits[i].result.primary_artist.name + '</td><td><a href="" data-song="'+ data.response.hits[i].result.id +'" data-artist="'+ data.response.hits[i].result.primary_artist.name +'" class="btn btn-primary"><i class="far fa-eye"></i></a></td></tr>');
+			tbody.append('<tr><td>' + data.response.hits[i].result.title + '</td><td>' + data.response.hits[i].result.primary_artist.name + '</td><td><a data-song="'+ data.response.hits[i].result.id +'" data-artist="'+ data.response.hits[i].result.primary_artist.id +'" class="btn btn-primary text-white button--view"><i class="far fa-eye"></i></a></td></tr>');
 			console.log('title: ' + data.response.hits[i].result.title + ' Artista: ' +  data.response.hits[i].result.primary_artist.name);
 
 		}
-		
 
 	}
 
-	function genius_get(url, query) {
+
+	function printModal(data) {
+		$('#modal--title').text(data.response.song.full_title);
+		$('#modal--imagen').attr('src', data.response.song.header_image_thumbnail_url);
+		$('#span--album').text(data.response.song.album.name);
+		$('#div--media').html('<a href="'+data.response.song.media[1].url+'" class="btn btn-success text-white "><i class="fab fa-spotify"></i></a>&nbsp<a href="'+data.response.song.media[0].url+'" class="btn btn-danger text-white"><i class="fab fa-youtube"></i></a>&nbsp<a href="'+data.response.song.url+'" class="btn btn-warning text-dark">Genius</a>');
+		$('#modal--song').modal();
+	}
+
+
+	function genius_get(url/*, query*/) {
 		return $.ajax({
 			url: 'https://genius.p.rapidapi.com' + url,
 			type: 'get',
 			dataType: 'json',
-			data:{
+			/*data:{
 				q: query
-			},
+			},*/
 			headers:{
 				'x-rapidapi-host': 'genius.p.rapidapi.com',
 				'x-rapidapi-key': 'a6ec3401a8mshcb4faf17e6eb123p137240jsnbe2edb4cd9ad'
